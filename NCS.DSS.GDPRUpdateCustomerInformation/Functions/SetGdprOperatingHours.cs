b1400f8594fb06cff483233bbdd1df7692d965e1
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-04-25 15:52:49 +0100

    Adding new function to set operating hours

---
+++ NCS.DSS.GDPRUpdateCustomerInformation/Functions/SetGdprOperatingHours.cs
@@ -0,0 +1,32 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace NCS.DSS.DataUtility.Functions
+{
+    public class SetGdprOperatingHours
+    {
+        private readonly ILogger _logger;
+
+        public SetGdprOperatingHours(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<SetGdprOperatingHours>();
+        }
+
+        [Function("SetGdprOperatingHours")]
+        public void Run([TimerTrigger("*/5 * * * *")] TimerInfo myTimer) // every 5 minutes
+        {
+            TimeSpan currentTime = DateTime.Now.TimeOfDay;
+            TimeSpan stopTime = new TimeSpan(16, 10, 0);
+            TimeSpan startTime = new TimeSpan(16, 20, 0);
+
+            if ((currentTime > stopTime) && (currentTime < startTime))
+            {
+                Environment.SetEnvironmentVariable("AzureWebJobs.DeleteCustomerData.Disabled", "true");
+            } 
+            else
+            {
+                Environment.SetEnvironmentVariable("AzureWebJobs.DeleteCustomerData.Disabled", "false");
+            }
+        }
+    }
+}
