7f41d7f2028851398b9c39e8ecd89643be5f573e
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-04-25 16:25:16 +0100

    WIP time zones

--- NCS.DSS.GDPRUpdateCustomerInformation/Functions/SetGdprOperatingHours.cs
+++ NCS.DSS.GDPRUpdateCustomerInformation/Functions/SetGdprOperatingHours.cs
@@ -15,20 +15,34 @@ namespace NCS.DSS.DataUtility.Functions
         [Function("SetGdprOperatingHours")]
         public void Run([TimerTrigger("*/5 * * * *")] TimerInfo myTimer) // every 5 minutes
         {
+            DateTime currentDateTimeUTC = DateTime.Now.ToUniversalTime();
             TimeSpan currentTime = DateTime.Now.TimeOfDay;
+
+            
+            
+            //var dt = new DateTime(localTime.Ticks);
+            //var utc = dt.ToUniversalTime();
+            //return new TimeSpan(utc.Ticks);
+
             TimeSpan stopTime = new TimeSpan(16, 10, 0);
             TimeSpan startTime = new TimeSpan(16, 20, 0);
+            TimeSpan timespanUTC = new TimeSpan(currentDateTimeUTC.Ticks);
 
             _logger.LogInformation($"CURRENT TIME: {currentTime.ToString()}");
+            _logger.LogInformation($"CURRENT TIME UTC: {currentDateTimeUTC.ToString()}");
 
-            if ((currentTime > stopTime) && (currentTime < startTime))
-            {
-                Environment.SetEnvironmentVariable("AzureWebJobs.DeleteCustomerData.Disabled", "true");
-            } 
-            else
-            {
-                Environment.SetEnvironmentVariable("AzureWebJobs.DeleteCustomerData.Disabled", "false");
-            }
+            _logger.LogInformation($"timespan UTC: {timespanUTC.ToString()}");
+            _logger.LogInformation($"stop: {stopTime.ToString()}");
+            _logger.LogInformation($"start: {startTime.ToString()}");
+
+            //if ((currentTime > stopTime) && (currentTime < startTime))
+            //{
+            //    Environment.SetEnvironmentVariable("AzureWebJobs.DeleteCustomerData.Disabled", "true");
+            //} 
+            //else
+            //{
+            //    Environment.SetEnvironmentVariable("AzureWebJobs.DeleteCustomerData.Disabled", "false");
+            //}
         }
     }
 }
