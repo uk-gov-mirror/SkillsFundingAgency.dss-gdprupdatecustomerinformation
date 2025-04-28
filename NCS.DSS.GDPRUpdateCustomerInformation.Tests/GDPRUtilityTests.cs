561d5a7d3cff618d2fea98b1ec464aebc121248c
Benjamin Matthews <ben.matthews@education.gov.uk>
2025-04-28 11:37:02 +0100

    Improvement to unit tests

--- NCS.DSS.GDPRUpdateCustomerInformation.Tests/GDPRUtilityTests.cs
+++ NCS.DSS.GDPRUpdateCustomerInformation.Tests/GDPRUtilityTests.cs
@@ -1,38 +1,52 @@
 using FakeItEasy;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using NCS.DSS.DataUtility.Functions;
+using NCS.DSS.DataUtility.Interfaces;
 
 namespace NCS.DSS.DataUtility.Tests
 {
     public class GDPRUtilityTests
     {
-        /*private readonly IIdentifyAndAnonymiseDataService _fakeDataService;
-        private readonly ILogger<Function.GDPRUtility> _fakeLogger;
-        private readonly Function.GDPRUtility _function;
+        private readonly ISqlDbService _mockedSqlDbService;
+        private readonly ICosmosDatabaseService _mockedCosmosDbService;
+        private readonly IServiceBusService _mockedServiceBusService;
+        private readonly ILogger<RetrieveCustomersToBeDeleted> _mockedRetrievalFunctionLogger;
+        private readonly RetrieveCustomersToBeDeleted _mockedRetrievalFunction;
 
         public GDPRUtilityTests()
         {
-            _fakeDataService = A.Fake<IIdentifyAndAnonymiseDataService>();
-            _fakeLogger = A.Fake<ILogger<Function.GDPRUtility>>();
-            _function = new Function.GDPRUtility(_fakeDataService, _fakeLogger);
+            _mockedSqlDbService = A.Fake<ISqlDbService>();
+            _mockedCosmosDbService = A.Fake<ICosmosDatabaseService>();
+            _mockedServiceBusService = A.Fake<IServiceBusService>();
+            _mockedRetrievalFunctionLogger = A.Fake<ILogger<RetrieveCustomersToBeDeleted>>();
+            _mockedRetrievalFunction = new RetrieveCustomersToBeDeleted(_mockedRetrievalFunctionLogger, _mockedSqlDbService, _mockedServiceBusService);
         }
 
         [Fact]
         public async Task Run_NoCustomers_NoOperationsPerformed()
         {
             // Arrange
-            A.CallTo(() => _fakeDataService.ReturnCustomerIds()).Returns(Task.FromResult(new List<Guid>()));
+            A.CallTo(() => _mockedSqlDbService.RetrieveCustomerIdsAsync()).Returns(Task.FromResult(new List<Guid>()));
             var timerInfo = new TimerInfo();
+            Guid customerId = Guid.NewGuid();
 
             // Act
-            await _function.RunAsync(timerInfo);
+            await _mockedRetrievalFunction.Run(timerInfo);
 
             // Assert
-            A.CallTo(() => _fakeDataService.AnonymiseData()).MustNotHaveHappened();
-            A.CallTo(() => _fakeDataService.DeleteCustomersFromCosmos(A<List<Guid>>.Ignored)).MustNotHaveHappened();
+            A.CallTo(() => _mockedSqlDbService.PurgeDataItemsForCustomerAsync(customerId)).MustNotHaveHappened();
+            A.CallTo(() => _mockedSqlDbService.PurgeCustomerDataAsync(customerId)).MustNotHaveHappened();
+
+            A.CallTo(() => _mockedCosmosDbService.PurgeActionPlansForCustomerAsync(customerId)).MustNotHaveHappened();
+
+            A.CallTo(_mockedRetrievalFunctionLogger).Where(call => 
+                call.Method.Name == "Log" 
+                && call.GetArgument<LogLevel>(0) == LogLevel.Information
+            ).MustHaveHappened(3, Times.Exactly);
         }
 
-        [Fact]
+        /*[Fact]
         public async Task Run_CustomersExist_OperationsPerformed()
         {
             // Arrange
